dc075de4a10850b2d26e82d4788ebb04ca573591
leon <719717747@qq.com>
2023-11-19 15:14:37 +0800

    增加icon

--- ExportDB/obj/Debug/net8.0-windows/ExportDB_b0fkt0c0_wpftmp.AssemblyInfo.cs
+++ ExportDB/obj/Debug/net8.0-windows/ExportDB_b0fkt0c0_wpftmp.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExportDB")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca525826141501d1a09b69f62f1c35d2658fc118")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExportDB")]
