e69ccd386f1cbfff5b3cc08c3998b77a42c473cf
leon <719717747@qq.com>
2023-11-19 16:12:39 +0800

    更新可执行文件

--- ExportDB/obj/Release/net8.0-windows/ExportDB.AssemblyInfo.cs
+++ ExportDB/obj/Release/net8.0-windows/ExportDB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExportDB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca525826141501d1a09b69f62f1c35d2658fc118")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2b132974803211060194367dfaddae8cb5af7d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExportDB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExportDB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
